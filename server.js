85315180a54d23a45e9491f6c5e109854a5d9d77
Ina Bjornestad <iinusen7@gmail.com>
2019-05-22 16:02:04 +0200

    solve problem with service worker, which is now sent from server side. All PWA requirments fullfilled in audit.

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var hbs = require('hbs');
 var app = express ();
 var dict = {};
 app.set ('view engine', 'hbs');
-app.use (express.static(__dirname + '/public'));
+app.use (express.static(__dirname + '/public/app.js'));
 
 var id = uuidv4();
-var trackID = '?id=' + id; 
+const trackID = id; 
 
-var obj = {
+var manifest = {
   "name": "PWA for tracking users",
   "short_name": "Tracking users",
   "description": "PWA that demonstrates user tracking",
@@ -26,7 +26,7 @@ var obj = {
       "sizes": "192x192"
       }
   ],
-  "start_url": trackID,
+  "start_url": '?id='+ trackID,
   "display": "standalone",
   "background_color": "#f0db4f",
   "theme_color": "#FFC0CB"
@@ -44,7 +44,7 @@ app.get ('/', (req, res) => {
           }});  
         } 
         else {
-        var json = JSON.stringify(obj); 
+        var json = JSON.stringify(manifest); 
         fs.writeFile('public/manifest.json', json, 'utf8', err => {
           if (err) {
               console.log('Error writing file', err)
@@ -61,28 +61,89 @@ app.get ('/', (req, res) => {
   }else{
     dict[id]=1
   }
+
   console.log(dict)
-  res.render('index.hbs', {trackID: req.params.trackID});
+  res.render('index.hbs', {trackID: trackID});
 })
 
-app.get ('/manifest.json', (req, res) => {
-  var matches = /\/([a-z]+)\/?$/i.exec (req.headers.referer);
-  if (matches && matches.length > 1) {
-    var trackID = matches[1];
-  } else {
-    var id = uuidv4();
-    var trackID = '?id=' + id; //Default
-  }
-  // Need to set content type, default is text/html
-  res.set ('Content-Type', 'application/json');
-  res.render ('manifest.hbs', {trackID});
-  //setTimeout(1000);
-  //res.redirect('/');
+app.get (':?id=' + trackID, (req, res) => {
+  res.render ('index.hbs', {trackID: trackID});
 });
 
-app.get ('/:trackID', (req, res) => {
-  res.render ('index.hbs');
-});
+app.get("/manifest.json", (req, res) => {
+  res.append("Content-Type", "text/json; charset=utf-8")
+  res.send(JSON.stringify(manifest))
+})
+
+app.get("/app.js", (req, res) => {
+  res.append("Content-Type", "text/javascript; charset=utf-8")
+  res.send(`
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register ('/sw.js')
+      .then (registration => console.log ('Service worker registration successful!' + registration.scope));
+  }
+  `)
+})
+
+app.get("/sw.js", (req, res) => {
+  res.append("Content-Type", "text/javascript; charset=utf-8")
+  res.send(`
+  var staticCacheName = 'pre-cache';
+  self.addEventListener('install', function (event) {
+    console.log('ServiceWorker (' + staticCacheName + '): install called');
+    event.waitUntil(
+      caches.open(staticCacheName).then(function (cache) {
+        return cache.addAll([
+          '/?id=${trackID}',
+          '/',
+
+        ]);
+      })
+    );
+  });
+  
+  //':/?id=' + '{{trackID}}'
+  //console.log("trackid" + {{trackID}});
+  
+  self.addEventListener('activate', function (event) {
+    console.log('ServiceWorker: Activate');
+    //activate active worker asap
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener('fetch', function (event) {
+    //handle live reload function (for develop purpose)
+    if (event.request.url.indexOf('/browser-sync/') !== -1) {
+      //fetch(..) is the new XMLHttpRequest
+      event.respondWith(fetch(event.request));
+      return;
+    }
+  
+    console.log('ServiceWorker: fetch called for ' + event.request.url);
+    //if request in cache then return it, otherwise fetch it from the network
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(\`[Service Worker] Push had this data: "\${event.data.text()}"\`);
+  
+    const title = 'Push notification';
+    const options = {
+      body: 'Yay it works.',
+      icon: 'images/jslogo.png',
+      badge: 'images/jslogo.png'
+    };
+  
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
+  `)
+})
 
 app.listen(3000, function()
 {console.log("server listening")})
